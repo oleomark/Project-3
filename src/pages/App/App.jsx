a413c1df9589098378cf950a62d9460f52f9b9b0
Mark Oleo <oleo.mark@gmail.com>
2021-05-02 15:57:03 -0700

    Fix set up issues

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,8 +5,8 @@ import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
-
 import './App.css';
+
 
 export default function App() {
 	const [user, setUser] = useState(getUser());
